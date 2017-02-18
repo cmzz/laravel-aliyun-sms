4a875d9bbb211e3dddca5c604ceb6bebcb67f212
wuzhuo <me19@qq.com>
2017-02-18 15:17:00 +0800

    0.1.4

--- src/AliyunSms.php
+++ src/AliyunSms.php
@@ -4,6 +4,9 @@ namespace Cmzz\LaravelAliyunSms;
 
 use Cmzz\AliyunCore\Profile\DefaultProfile;
 use Cmzz\AliyunCore\DefaultAcsClient;
+use Cmzz\AliyunCore\Regions\Endpoint;
+use Cmzz\AliyunCore\Regions\EndpointConfig;
+use Cmzz\AliyunCore\Regions\EndpointProvider;
 use Cmzz\AliyunSms\Sms\Request\V20160927\SingleSendSmsRequest;
 use Cmzz\AliyunCore\Exception\ClientException;
 use Cmzz\AliyunCore\Exception\ServerException;
@@ -12,7 +15,13 @@ class AliyunSms {
 
     public function send($mobile, $tplId, $params)
     {
-        dd(config());
+        define('ENABLE_HTTP_PROXY', env('ALIYUN_SMS_ENABLE_HTTP_PROXY', false));
+        define('HTTP_PROXY_IP',     env('ALIYUN_SMS_HTTP_PROXY_IP', '127.0.0.1'));
+        define('HTTP_PROXY_PORT',   env('ALIYUN_SMS_HTTP_PROXY_PORT', '8888'));
+
+        $endpoint = new Endpoint(config('aliyunsms.region_id'), EndpointConfig::getregionIds(), EndpointConfig::getProducDomains());
+        $endpoints = array($endpoint);
+        EndpointProvider::setEndpoints($endpoints);
 
         $iClientProfile = DefaultProfile::getProfile(config('aliyunsms.region_id'), config('aliyunsms.access_key'), config('aliyunsms.access_secret'));
         $client = new DefaultAcsClient($iClientProfile);
--- src/config.php
+++ src/config.php
@@ -1,8 +1,5 @@
 <?php
 
-define('ENABLE_HTTP_PROXY', env('ALIYUN_SMS_ENABLE_HTTP_PROXY', false));
-define('HTTP_PROXY_IP',     env('ALIYUN_SMS_HTTP_PROXY_IP', '127.0.0.1'));
-define('HTTP_PROXY_PORT',   env('ALIYUN_SMS_HTTP_PROXY_PORT', '8888'));
 
 return [
 
